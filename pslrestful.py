fedb58ad6008f715f8267efd78512d890a5afb32
Pontus Hedman <phedman@perle.com>
2024-04-05 11:53:08 -0400

    Robustness improvements.
    - Add automatic retry with backoff.
    - Add logcb option so you can see retries (e.g. via logcb=print)
    - Give get/put/port same signature for consistency

--- pslrestful.py
+++ pslrestful.py
@@ -6,6 +6,7 @@ Provides get/put/post methods and automatic authorization.
 Look at the main() for sample code.
 """
 import os
+import time
 import requests
 import tempfile
 import pickle
@@ -24,16 +25,25 @@ class PSL_RESTfulAPI:
     _COOKIEFILE = os.path.join(os.getenv('HOME', tempfile.gettempdir()),
                                '.pslcookie')
 
-    def __init__(self, username, password, host,
-                 port=8080, schema='http', ver='v1.1'):
+    def __init__(self, username, password, host, port=8080,
+                 schema='http', ver='v1.1', retries=10, logcb=None):
         '''
         Initialize with given username/password at host.
-        port, schema and ver are optional.
+
+        port: port to use
+        schema: http or https
+        ver: API version
+        retries: how many connection retry attempts
+        logcb: a log callback function which takes a single str.
         '''
         self.url_prefix = f'{schema}://{host}:{port}/api/{ver}/managed-devices'
         self.username = username
         self.password = password
         self.cookie = None
+        self.retries = max(1, retries)
+        if logcb is not None and not callable(logcb):
+            raise TypeError('logcb must be callable')
+        self.logcb = logcb
         self.loadcookie()
         if not self.cookie:
             self.login()
@@ -46,7 +56,26 @@ class PSL_RESTfulAPI:
                     (b'User is not authorized' in resp.content))
 
         args['cookies'] = self.cookie
-        resp = op(**args)
+
+        # Try the request, retrying on any IO error (RESTful probably down)
+        sleeptime = 1
+        for retry in range(self.retries):
+            conerr = None
+            try:
+                resp = op(**args)
+                break
+            except (ConnectionError, requests.RequestException) as e:
+                if self.logcb is not None:
+                    self.logcb(f'Connection failed - retrying '
+                               f'({retry} of {self.retries}): {e}')
+                conerr = e
+                time.sleep(sleeptime)
+                sleeptime = min(sleeptime*2, 16)
+
+        if conerr is not None:
+            # Give up
+            raise conerr
+
         if badlogin(resp):
             # Redo login and try again
             self.login()
@@ -54,21 +83,22 @@ class PSL_RESTfulAPI:
             resp = op(**args)
             if badlogin(resp):
                 raise Exception(resp.content)
+
         return resp
 
-    def get(self, url):
-        '''Perform a GET'''
+    def get(self, url, cmd={}):
+        '''Perform a GET.  cmd unused here.'''
         api_url = self.url_prefix + url
         return self._requestop(requests.get, {'url': api_url})
 
-    def put(self, url, cmd):
+    def put(self, url, cmd={}):
         '''Perform a PUT'''
         if not isinstance(cmd, dict):
             raise TypeError('dict-like object required for cmd')
         api_url = self.url_prefix + url
         return self._requestop(requests.put, {'url': api_url, 'json': cmd})
 
-    def post(self, url, cmd):
+    def post(self, url, cmd={}):
         '''Perform a POST'''
         if not isinstance(cmd, dict):
             raise TypeError('dict-like object required for cmd')
